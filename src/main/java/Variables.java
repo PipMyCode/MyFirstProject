8449dba7f2e3ede4dbebf2d75e0709b1f0fb0d74
marcusadigun <adigunmarcus@gmail.com>
2026-05-04 09:07:06 -0400

    feat: add double variables and print statements
    Comment out integer variable print statements and introduce double
    variables (price, cgpa, temperature) with corresponding console output

--- src/main/java/Variables.java
+++ src/main/java/Variables.java
@@ -6,8 +6,20 @@ public class Variables {
         int year = 2026;
         int quantity = 1;
 
+        /*
+
+
         System.out.println("I am " + age + " years of age");
         System.out.println("The year is " + year);
         System.out.println("The quantity of the flour is " + quantity);
+        */
+
+        double price = 50.99;
+        double cgpa = 3.69;
+        double temperature = -30.2;
+
+        System.out.println("The price of the item is $" + price);
+        System.out.println("My cgpa is " + cgpa);
+        System.out.println("Today's temperature is exactly " + temperature);
     }
 }
