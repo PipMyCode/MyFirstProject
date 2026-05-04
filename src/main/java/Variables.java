cf3166401d1863023fa143d8be2e1226d8e041fb
marcusadigun <adigunmarcus@gmail.com>
2026-05-04 09:43:40 -0400

    feat: add char, boolean and string variables with updated output
    - Add char variables (grade, symbol, currency)
    - Add boolean variables (isStudent, forSale, isOnline)
    - Add String variables (name, food, email, car, color)
    - Update age and cgpa values
    - Replace old print statements with new comprehensive output

--- src/main/java/Variables.java
+++ src/main/java/Variables.java
@@ -2,7 +2,7 @@ public class Variables {
     public static void main(String[] args) {
 
 
-        int age = 22;
+        int age = 19;
         int year = 2026;
         int quantity = 1;
 
@@ -15,11 +15,50 @@ public class Variables {
         */
 
         double price = 50.99;
-        double cgpa = 3.69;
+        double cgpa = 4.69;
         double temperature = -30.2;
-
+        /*
         System.out.println("The price of the item is $" + price);
         System.out.println("My cgpa is " + cgpa);
         System.out.println("Today's temperature is exactly " + temperature);
+        */
+
+        char grade = 'A';
+        char symbol = '!';
+        char currency = '$';
+
+        /*
+        System.out.println("You got an " + grade + " in your Math course");
+        System.out.println("Hey" + symbol);
+        System.out.println("The official currency of the USA is " + currency);
+        */
+
+        boolean isStudent = false;
+        boolean forSale = false;
+        boolean isOnline = true;
+
+        /*
+        if(isStudent) {
+            System.out.println("You are a student!");
+        }
+        else{
+            System.out.println("You are not a student!");
+        }
+        */
+
+        String name = "Damola";
+        String food = "Amala";
+        String email = "blablabla123@gmail.com";
+        String car = "Mustang Dark Horse";
+        String color = "Orange";
+
+        System.out.println("My name is " + name);
+        System.out.println("I am " + age + " years old");
+        System.out.println("My cgpa is: " + cgpa);
+        System.out.println("I got an " + grade + " in Maths");
+        System.out.println("My favorite food is " + food);
+        System.out.println("My email is " + email);
+        System.out.println("My dream car is a " + car);
+        System.out.println("In the year " + year + ", I got an " + color + " " + car + " and I registered it with " + name + " name");
     }
 }
