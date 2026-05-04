95c02c085f0ff24611f398fde78ca39c21a9a054
marcusadigun <adigunmarcus@gmail.com>
2026-05-04 06:06:29 -0400

    Added more print statements.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,24 @@
 public class Main {
 
     public static void main(String[] args) {
+        // This IS MY FIRST JAVA PROGRAM
 
+        /*
+            This
+            is
+            a
+            multi-line
+            comment
+
+         */
         System.out.println("I like Jollof rice");
         System.out.println("It's really good!");
         System.out.println("Buy me Jollof rice");
+
+        System.out.println("I am learning Java to be a Backend Engineer");
+        System.out.println("I also just started, so i hope i get the hang of this");
+
+
     }
 
 }
