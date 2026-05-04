6cc5d61329624a8dc8d7e88991a086c6576671da
marcusadigun <adigunmarcus@gmail.com>
2026-05-04 05:58:41 -0400

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("I like Jollof rice");
+        System.out.println("It's really good!");
+        System.out.println("Buy me Jollof rice");
+    }
+
+}
