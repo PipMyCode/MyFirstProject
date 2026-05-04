386cee1a1db5618ad94a296a211fa17389b28413
marcusadigun <adigunmarcus@gmail.com>
2026-05-04 09:01:13 -0400

    feat: add main method with variable declarations

---
+++ src/main/java/Variables.java
@@ -0,0 +1,13 @@
+public class Variables {
+    public static void main(String[] args) {
+
+
+        int age = 22;
+        int year = 2026;
+        int quantity = 1;
+
+        System.out.println("I am " + age + " years of age");
+        System.out.println("The year is " + year);
+        System.out.println("The quantity of the flour is " + quantity);
+    }
+}
