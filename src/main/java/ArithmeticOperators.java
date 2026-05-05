20400663327c12b2ee46b7cb4fa19505feb322a4
marcusadigun <adigunmarcus@gmail.com>
2026-05-05 06:48:09 -0400

    feat: implement arithmetic operators demonstration

---
+++ src/main/java/ArithmeticOperators.java
@@ -0,0 +1,18 @@
+public class ArithmeticOperators {
+    public static void main(String[] args) {
+
+        int x = 20;
+        int y = 5;
+        int z;
+
+        // z = x + y;
+        // z = x - y;
+        // z = x * y;
+        // z = x / y;
+        z = x % y;
+
+
+        System.out.println(z);
+
+    }
+}
