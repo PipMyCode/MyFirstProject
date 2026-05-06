17ebe673a4d47c7550991aeb5585e4be9f8f80b5
marcusadigun <adigunmarcus@gmail.com>
2026-05-06 07:35:42 -0400

    refactor: update arithmetic demo to show order of operations

--- src/main/java/ArithmeticOperators.java
+++ src/main/java/ArithmeticOperators.java
@@ -1,17 +1,11 @@
 public class ArithmeticOperators {
     public static void main(String[] args) {
 
-        int x = 33;
-        int y = 5;
+        // ORDER OF OPERATIONS [P-E-M-D-A-S]
 
+        double result = 4 + 4 * (7 - 5) / 2.0;
 
-
-        System.out.println(x + y);
-        System.out.println(x - y);
-        System.out.println(x / y);
-        System.out.println(x * y);
-        System.out.println(x % y);
-
+        System.out.println(result);
 
 
     }
