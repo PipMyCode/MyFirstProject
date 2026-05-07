f3fd983e8755dfe005d21fcfc2b8e3b7c4eb6af2
marcusadigun <adigunmarcus@gmail.com>
2026-05-07 10:04:17 -0400

    refactor(CompoundInterest): rename scanner variable from s to input

--- src/main/java/CompoundInterest.java
+++ src/main/java/CompoundInterest.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class CompoundInterest {
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
         // Compound interest calculator
 
@@ -13,22 +13,22 @@ public class CompoundInterest {
         double amount;
 
         System.out.print("Enter the principal amount: ");
-        principal = s.nextDouble();
+        principal = input.nextDouble();
 
         System.out.print("Enter the interest rate (in %): ");
-        rate = s.nextDouble() / 100;
+        rate = input.nextDouble() / 100;
 
         System.out.print("Enter the # of times compounded per year: ");
-        timeCompounded = s.nextInt();
+        timeCompounded = input.nextInt();
 
         System.out.print("Enter the # of years: ");
-        years = s.nextInt();
+        years = input.nextInt();
 
         amount = principal * Math.pow(1 + rate / timeCompounded, timeCompounded * years);
 
         System.out.printf("The amount after %d years is N%,.2f", years, amount);
 
 
-        s.close();
+        input.close();
     }
 }
