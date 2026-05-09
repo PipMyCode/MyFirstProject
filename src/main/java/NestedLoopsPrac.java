41c3319883eee44ac5707d99fb357c39289f48dc
marcus <adigunmarcus@gmail.com>
2026-05-09 16:49:38 -0400

    chore: for nested loop practice

---
+++ src/main/java/NestedLoopsPrac.java
@@ -0,0 +1,14 @@
+public class NestedLoopsPrac {
+    public static void main(String[] args) {
+        // nested loop =  A loop inside another loop
+        //                  used often with metrices or DS&A
+
+        for (int i = 1; i <= 3; i++) {
+            for (int j = 1; j <= 9; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
