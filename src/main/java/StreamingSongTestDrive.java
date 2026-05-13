485c77a2b1d1a8c7d5a82f49336238712915a3b9
PipMyCode <adigunmarcus@gmail.com>
2026-05-13 15:11:11 -0400

    feat: add StreamingSong class and basic test drive for practicing objects and methods

---
+++ src/main/java/StreamingSongTestDrive.java
@@ -0,0 +1,11 @@
+public class StreamingSongTestDrive {
+    public static void main(String[] args) {
+
+        StreamingSong song = new StreamingSong();
+
+        song.artist = "Davido";
+        song.title = "Skelewu";
+        song.play();
+        song.printDetails();
+    }
+}
