343200306b8b14b147758fe1f71e9aa55608cc88
PipMyCode <adigunmarcus@gmail.com>
2026-05-16 03:08:58 -0400

    feat: add InterfaceTestDrive class to practice interface implementation with Rabbit, Hawk, and Crab classes

---
+++ src/main/java/InterfaceTestDrive.java
@@ -0,0 +1,13 @@
+public class InterfaceTestDrive {
+    public static void main(String[] args) {
+
+        Rabbit rabbit = new Rabbit();
+        Hawk hawk = new Hawk();
+        Crab crab = new Crab();
+
+        crab.flee();
+        crab.hunt();
+
+
+    }
+}
